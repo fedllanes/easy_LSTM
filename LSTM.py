1cda8a13dc6fa19b5b785486237b208c38f85e49
federicol <federicol@unlimiteck.com>
2021-06-11 22:48:48 +0200

    SEcond version.
    Improved code

--- LSTM.py
+++ LSTM.py
@@ -1,48 +1,58 @@
 import numpy as np
+import pandas as pd
 
 
 class EasyLSTM:
-    def __init__(self, make_model, n_steps=20, n_features=1):
+    def __init__(self, make_model, n_steps=20):
         self.n_steps = n_steps
-        self.n_features = n_features
+        self.n_features = None
         self.model = make_model(self.n_steps, self.n_features)
-        self.X = []
-        self.history = []
+        self.X = None
+        self.history = None
+        self.columns = None
 
-    def fit(self, dataset, train_test_split=0, epochs=20, verbose=0):
-        X, y = self._treat_dataframe(dataset, self.n_steps)
-        self.X = X
-        self.history = self.model.fit(X, y, epochs=epochs, verbose=verbose)
+    def fit(self, dataset, train_test_split=0, epochs=20, verbose=0, *args, **kwargs):
+        if not isinstance(dataset, pd.DataFrame):
+            dataset = pd.DataFrame(dataset)
+        self.columns = dataset.columns
+        self.n_features = dataset.shape[1]
+        validation_data = None
+        if train_test_split > 0:
+            df_test = dataset[-int(len(dataset) * train_test_split):]
+            validation_data = self._treat_dataframe(df_test)
+            dataset = dataset[:int(len(dataset) * train_test_split)]
+        X, y = self._treat_dataframe(dataset)
+        self.X = X if train_test_split == 0 else validation_data[0]
+        self.history = self.model.fit(X, y, epochs=epochs, verbose=verbose, validation_data=validation_data, *args,
+                                      **kwargs)
 
     def predict(self, n_predictions):
         X_entended = self._make_prediction(n_predictions)
         predictions = self._inverse_logic(X_entended)[-n_predictions:]
+        predictions = pd.DataFrame(np.array(predictions), columns=self.columns)
         return predictions
 
-    def _treat_dataframe(self, dataset, n_steps):
+    def _treat_dataframe(self, dataset):
         X_to_concat = []
         y_to_concat = []
         for element in dataset:
-            series_x, series_y = self._split_sequence(dataset[element], n_steps)
+            series_x, series_y = self._split_sequence(dataset[element], self.n_steps)
             series_y = series_y.reshape((series_y.shape[0], 1))
             series_x = series_x.reshape((series_x.shape[0], series_x.shape[1], 1))
             X_to_concat.append(series_x)
             y_to_concat.append(series_y)
         X = np.concatenate(X_to_concat, axis=2)
         y = np.concatenate(y_to_concat, axis=1)
-        return X,y
-
+        return X, y
 
     @staticmethod
     def _split_sequence(sequence, n_steps):
         X, y = list(), list()
+        sequence = sequence.reset_index(drop=True)
         for i in range(len(sequence)):
-            # Encontrar el final de la secuencia
             end_ix = i + n_steps
-            # Verificar que no nos hayamos pasado
             if end_ix > len(sequence) - 1:
                 break
-            # Agregar los valores a las salidas
             seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
             X.append(seq_x)
             y.append(seq_y)
@@ -50,17 +60,13 @@ class EasyLSTM:
 
     @staticmethod
     def _inverse_logic(X):
-        new = []  # Lista donde se guardará el array de los valores de entrenamiento
-        # sumado a lo predicho
-        for i in range(X.shape[0]):  # Iteramos sobre el primer eje, es decir la cantidad
-            # de listas de listas que tenemos
-            new.append(X[i][0])  # Guardamos el primer elemento de cada eje
-        for j in range(X.shape[1]):  # Iteramos sobre el segundo eje, es decir
-            # la cantidad de elementos que vemos hacía el pasado
+        new = []
+        for i in range(X.shape[0]):
+            new.append(X[i][0])
+        for j in range(X.shape[1]):
             if j == 0:
-                continue  # Este elemento ya fue agregado
-            new.append(X[i][j])  # Agregamos cada elemento del último elemento de la
-            # lista de lista a nuestra nueva lista
+                continue
+            new.append(X[i][j])
         return new
 
     def _make_prediction(self, n_predictions):
@@ -69,6 +75,5 @@ class EasyLSTM:
             x_input = X_new[-1].reshape((1, self.n_steps, self.n_features))
             yhat = self.model.predict(x_input, verbose=0)
             new_element = np.concatenate([X_new[-1][1:, :].reshape(self.n_steps - 1, self.n_features), yhat])
-            X_new = np.append(X_new, new_element.reshape(1, self.n_steps, self.n_features),
-                              axis=0)
+            X_new = np.append(X_new, new_element.reshape(1, self.n_steps, self.n_features), axis=0)
         return X_new
